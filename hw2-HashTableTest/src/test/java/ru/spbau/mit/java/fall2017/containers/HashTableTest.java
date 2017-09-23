52ce182026efa9b5e7efade5a1d7c3a5974b22c5
Dmitriy Abramov <karvozavr@gmail.com>
2017-09-23 23:58:59 +0300

    Add more tests. Full coverage.

--- hw2-HashTableTest/src/test/java/ru/spbau/mit/java/fall2017/containers/HashTableTest.java
+++ hw2-HashTableTest/src/test/java/ru/spbau/mit/java/fall2017/containers/HashTableTest.java
@@ -21,23 +21,102 @@ class HashTableTest {
     }
 
     @Test
-    void contains() {
+    void testContainsPositive() {
+        HashTable table = new HashTable();
+        String key = "key";
+        table.put(key, "value");
+        for (int i = 0; i < 8; i++) {
+            assertEquals(true, table.contains(key));
+        }
     }
 
     @Test
-    void get() {
+    void testContainsNegative() {
+        HashTable table = new HashTable();
+        for (int i = 0; i < 8; i++) {
+            assertEquals(false, table.contains("key"));
+        }
     }
 
     @Test
-    void put() {
+    void testContainsBase() {
+        HashTable table = new HashTable();
+        String key = "key";
+        table.put(key, "value");
+        assertEquals(true, table.contains(key));
+        assertEquals(false, table.contains("Other key."));
     }
 
     @Test
-    void remove() {
+    void testGetNull() {
+        HashTable table = new HashTable();
+        for (int i = 0; i < 8; i++) {
+            assertEquals(null, table.get("key"));
+        }
     }
 
     @Test
-    void clear() {
+    void testGetBase() {
+        HashTable table = new HashTable();
+        String key = "key";
+        String value = "value";
+        table.put(key, value);
+        for (int i = 0; i < 8; i++) {
+            assertEquals(value, table.get("key"));
+            assertEquals(null, table.get("Other key"));
+        }
+    }
+
+    @Test
+    void testPut() {
+        HashTable table = new HashTable();
+        String key = "key";
+        String value = "value";
+        table.put(key, value);
+        assertEquals(true, table.contains(key));
+        assertEquals(value, table.get(key));
+        assertEquals(false, table.contains("Other key."));
+    }
+
+    @Test
+    void testPutExisting() {
+        HashTable table = new HashTable();
+        String key = "key";
+        table.put(key, "value");
+        assertEquals("value", table.put(key, "value2"));
+        assertEquals(true, table.contains(key));
+        assertEquals("value2", table.get(key));
+    }
+
+    @Test
+    void testRemoveBase() {
+        HashTable table = new HashTable();
+        String key = "key";
+        table.put(key, "value");
+        assertEquals("value", table.remove(key));
+        assertEquals(false, table.contains(key));
+    }
+
+    @Test
+    void testRemoveNotInTable() {
+        HashTable table = new HashTable();
+        String key = "key";
+        table.put(key, "value");
+        assertEquals(null, table.remove("other key"));
+        assertEquals(true, table.contains(key));
+    }
+
+    @Test
+    void testClear() {
+        HashTable table = new HashTable();
+        for (int i = 0; i < 100; i++) {
+            table.put("key" + i, "val" + i);
+        }
+        table.clear();
+        assertEquals(0, table.size());
+        for (int i = 0; i < 100; i++) {
+            assertEquals(false, table.contains("key" + i));
+        }
     }
 
 }
