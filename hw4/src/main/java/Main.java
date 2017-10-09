c67fb5d94a0753cbbdcc85738f02dab777d3dda0
Dmitriy Abramov <karvozavr@gmail.com>
2017-10-09 21:08:07 +0300

    Add extractByRegex functionality for directory.

--- hw4/src/main/java/Main.java
+++ hw4/src/main/java/Main.java
@@ -8,11 +8,14 @@ public class Main {
      * main function
      * @param args first argument must be path to archive, second - regexp
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         try {
             ZipUtils.extractByRegex(args[0], args[1]);
         } catch (IOException e) {
-            e.printStackTrace();
+            System.err.println("Something has terribly crashed in IO.");
+            throw e;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.err.println("Invalid arguments!");
         }
     }
 }
--- hw4/src/main/java/ru/spbau/mit/karvozavr/ZipUtils.java
+++ hw4/src/main/java/ru/spbau/mit/karvozavr/ZipUtils.java
@@ -2,26 +2,50 @@ package ru.spbau.mit.karvozavr;
 
 import java.io.IOException;
 import java.nio.file.*;
+import java.util.stream.Stream;
 
 public class ZipUtils {
 
     /**
-     * Extract all files that match regexp from zip archive.
-     * @param zipFileName path to archive
-     * @param regex regular expression to match
+     * Extract all files that match regexp from given zip archive.
+     *
+     * @param zipFile path to archive
+     * @param regex   regular expression to match
      * @throws IOException in case of some troubles with files
      */
-    public static void extractByRegex(String zipFileName, String regex) throws IOException {
-        Path zipFile = Paths.get(zipFileName);
+    private static void extractByRegexFromArchive(Path zipFile, String regex) throws IOException {
         try (final FileSystem fileSystem = FileSystems.newFileSystem(zipFile, null)) {
             PathMatcher matcher = fileSystem.getPathMatcher("regex:" + regex);
 
-            Files.walk(fileSystem.getRootDirectories().iterator().next())
-                    .filter(file -> !Files.isDirectory(file))
-                    .filter(matcher::matches)
-                    .forEach(file -> {
+            try (Stream<Path> archiveRoot = Files.walk(fileSystem.getRootDirectories().iterator().next())) {
+                archiveRoot.filter(file -> !Files.isDirectory(file))
+                        .filter(matcher::matches)
+                        .forEach(file -> {
+                            try {
+                                Files.copy(file, Paths.get(file.getFileName().toString()));
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        });
+            }
+        }
+    }
+
+    /**
+     * Extract all files that match regexp from all zip archives in given directory.
+     *
+     * @param path  directory to work in
+     * @param regex regular expression to match
+     * @throws IOException in case of some troubles with files
+     */
+    public static void extractByRegex(String path, String regex) throws IOException {
+        try (Stream<Path> directory = Files.walk(Paths.get(path))) {
+            directory
+                    .filter(Files::isRegularFile)
+                    .filter(file -> !file.getFileName().toString().endsWith(".zip"))
+                    .forEach(archive -> {
                         try {
-                            Files.copy(file, Paths.get(file.getFileName().toString()));
+                            extractByRegexFromArchive(archive, regex);
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
