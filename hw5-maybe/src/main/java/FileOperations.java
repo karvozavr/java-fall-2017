3e7042987347bb03729905bc56d4de5218014a5a
Dmitriy Abramov <karvozavr@gmail.com>
2017-10-17 13:27:04 +0300

    File operations done.

---
+++ hw5-maybe/src/main/java/FileOperations.java
@@ -0,0 +1,52 @@
+import org.jetbrains.annotations.NotNull;
+import ru.spbau.mit.java.karvozavr.maybe.Maybe;
+import ru.spbau.mit.java.karvozavr.maybe.exception.MaybeIsNothingException;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class FileOperations {
+
+    /**
+     * Get all the numbers from the input file and save them squared to the output file.
+     * @param inputFileName input file name
+     * @param outputFileName output file name
+     */
+    public static void squareNumbers(@NotNull String inputFileName, @NotNull String outputFileName) {
+        try (Stream<String> lines = Files.lines(Paths.get(inputFileName));
+             PrintStream outStream = new PrintStream(outputFileName)) {
+            lines
+                    .map(FileOperations::parseIntToMaybe)
+                    .map(x -> x.map(a -> a * a))
+                    .filter(Maybe::isPresent)
+                    .forEach(x -> {
+                        try {
+                            outStream.println(x.get());
+                        } catch (MaybeIsNothingException e) {
+                            System.err.println("There has to be no exceptions!");
+                        }
+                    });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Parse int to Maybe
+     * @param s input string
+     * @return parsed Just value or Nothing
+     */
+    @NotNull
+    private static Maybe<Integer> parseIntToMaybe(@NotNull String s) {
+        Integer value;
+        try {
+            value = Integer.parseInt(s);
+            return Maybe.just(value);
+        } catch (NumberFormatException e) {
+            return Maybe.nothing();
+        }
+    }
+}
---
+++ hw5-maybe/src/test/java/ru/spbau/mit/java/karvozavr/maybe/MaybeTest.java
@@ -0,0 +1,12 @@
+package ru.spbau.mit.java.karvozavr.maybe;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MaybeTest {
+    @Test
+    void get() {
+    }
+
+}
